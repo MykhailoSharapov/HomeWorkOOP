751e6a8a6154420300642e14c3a338aafbee665d
Mykhailo Sharapov <mikesharapov1@gmail.com>
2020-11-28 00:36:11 +0200

    Create sweet Comparer

---
+++ HomeWorkOOP/SweetComparer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkOOP
+{
+    class SweetComparer : IComparer<Sweet>
+    {
+        public int Compare(Sweet s1, Sweet s2)
+        {
+            if (s1.SugarContentPercent > s2.SugarContentPercent)
+                return 1;
+            else if (s1.SugarContentPercent < s2.SugarContentPercent)
+                return -1;
+            else
+                return 0;
+        }
+    }
+}
